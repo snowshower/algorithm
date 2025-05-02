183f0a40037107b7472eeb890b1822e37e85517c
snow___shower <wonsb25@gmail.com>
2025-05-02 23:31:14 +0900

    [Silver III] Title: 수리공 항승, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+int main() {
+	//입출력 속도 향상
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	vector<int> v;
+	int N, L, w, cnt = 0, result = 1, tmp = 0;
+	cin >> N >> L;
+	for (int i = 0; i < N; i++) {
+		cin >> w;
+		v.push_back(w);
+	}
+	sort(v.begin(), v.end());
+	tmp = v[0] + L - 1;
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] > tmp) {
+			tmp = v[i] + L - 1;
+			result++;
+		}
+	}	
+
+	cout << result << '\n';
+	
+	return 0;
+}
