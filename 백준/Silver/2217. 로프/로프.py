e154e949c3d5cbaf2f70d2824120c1069a10c545
snow___shower <wonsb25@gmail.com>
2025-08-13 23:43:25 +0900

    [Silver IV] Title: 로프, Time: 84 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,14 +1,15 @@
 import sys
-import bisect
 input=sys.stdin.readline
 
 N=int(input())
-rope=[int(input()) for i in range(N)]
-rope.sort()
+rope=sorted(int(input()) for i in range(N))
+# rope=[int(input()) for i in range(N)]
+# rope.sort()
 
-w=rope[0]*N
-for i in range(1, len(rope)):
-    if rope[i]*(N-i)>w:
-        w=rope[i]*(N-i)
+w=max(rope[i]*(N-i) for i in range(N))
+# w=rope[0]*N
+# for i in range(1, N):
+#     if rope[i]*(N-i)>w:
+#         w=rope[i]*(N-i)
 
 print(w)
