f2e3bcce83ace5dc01c559afe7dc5e12b86ad344
snow___shower <wonsb25@gmail.com>
2025-08-14 17:26:51 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 51888 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -3,14 +3,16 @@ from collections import deque
 input=sys.stdin.readline
 
 N=int(input())
-queue=deque()
-for i in range(N):
-    queue.append(i+1)
+queue=deque(range(1, N+1))
+# queue=deque()
+# for i in range(N):
+#     queue.append(i+1)
 
 while len(queue)>1:
     queue.popleft()
-    front=queue[0]
-    queue.popleft()
-    queue.append(front)
+    queue.append(queue.popleft())
+    # front=queue[0]
+    # queue.popleft()
+    # queue.append(front)
 
 print(queue.popleft())
