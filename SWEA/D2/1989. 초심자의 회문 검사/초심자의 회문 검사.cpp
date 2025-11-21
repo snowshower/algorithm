4293c0c955ff3866b224174c3809f2f6c7c101e3
snow___shower <wonsb25@gmail.com>
2025-11-21 23:47:42 +0900

    [D2] Title: 초심자의 회문 검사, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);	
+
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		string s;
+		cin >> s;
+		
+		int ans = 0;
+
+		for (int i = 0, j = s.length() - 1; i < s.length(); i++, j--) {
+			if (s[i] == s[j])
+				ans = 1;
+			else
+				ans = 0;
+		}
+
+		cout << "#" << test_case << " " << ans << "\n";
+	}
+	return 0;
+}
